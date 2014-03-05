cbf7331490ef597a65972e3e3c4851b47e887a74
Ashvala Vinay <mail@ashvala.net>
2014-03-05 13:28:00 -0500

    Updated!

--- javascripts/jquery_addition.js
+++ javascripts/jquery_addition.js
@@ -1,23 +1,16 @@
-vanish_everything = function(delaynum){
-    $(".number").each(function(){
-        $(this).fadeOut(100).delay(delaynum);
-    });
-}
-fade_in_answer = function(added){
-    resultstr = "The result is: " + added;
-    $(".resultnum").text(resultstr)
-    $(".resultnum").fadeIn("slow").delay(300);
-}
-add_num = function(num1, num2, delayval){
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    var added = num1 + num2;
-    vanish_everything(delayval);
-    fade_in_answer(added);
-}
+(function($){
+    //Get Number
+    $.fn.getNum = function(){
+        return parseFloat($(this).text());
+    }
 
-$(document).ready(function(){
-$(".addkey").click(function(){
-    add_num($(".number1").text(), $(".number2").text(),400);
-});
-});
+    //Add them up
+    $.fn.addEmUp = function(){
+        var num = 0;
+        $(this).each(function(){
+            num += $(this).getNum();
+        });
+        return num;
+    }
+
+}(jQuery));
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+vanish_everything = function(delaynum){
+    $(".button").each(function(){
+        $(this).fadeOut(100).delay(delaynum);
+    });
+}
+fade_in_answer = function(answer){
+    resultstr = "The result is: " + answer;
+    $(".resultnum").text(resultstr)
+    $(".resultnum").fadeIn("slow").delay(300);
+}
+
+$(document).ready(function(){
+    $(".addkey").click(function(){
+//        add_num($(".number1").getNum(), $(".number2").getNum(),400);
+        vanish_everything(300);
+        fade_in_answer($(".number").addEmUp());
+
+    });
+});
